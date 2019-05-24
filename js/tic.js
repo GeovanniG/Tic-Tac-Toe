d64b465e8681aaece8663891a8c3a2295764161f
GeovanniG <32276348+GeovanniG@users.noreply.github.com>
2019-05-23 21:42:50 -0700

    Added event handlers to td's in table

--- js/tic.js
+++ js/tic.js
@@ -1,1 +1,10 @@
-alert();
+const table = document.querySelector("table");
+table.addEventListener("click", () => {
+	// Obtain the closest nested td on area clicked
+	const td = event.target.closest('td');
+ 
+ 	// No nested td on area clicked will make td == null
+	if (!td) return;
+	
+	td.style.backgroundColor = "red";
+});
