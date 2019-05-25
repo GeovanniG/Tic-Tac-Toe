b6c47568f4657b723b66c282c95ba0df22936e71
GeovanniG <32276348+GeovanniG@users.noreply.github.com>
2019-05-25 10:56:48 -0700

    Changed game logic giving every player their own array

--- js/tic.js
+++ js/tic.js
@@ -1,36 +1,40 @@
-// Arrays containing the squares of each player
-const player1 = [];
-const player2 = [];
+"use strict";
+
 // Dimensions of board
 const rows = 3;
 const cols = 3;
 const dim = rows*cols;
 // Board layout
-const isSquareAvailable = Array(dim).fill(true);
+const player1 = Array(dim).fill(false);
+const player2 = Array(dim).fill(false);
 const adjacentSquaresNeededToWin = 3;
-
+// Initial conditions
 let isPlayer1Turn = true;
 let isGameOver = false;
+let isSquareAvailable = false;
 
 for (let i = 0; i < dim; i++) {
 	document.querySelectorAll("td")[i].addEventListener("click", function () {
-		if (isSquareAvailable[i] && !isGameOver) {
-			isSquareAvailable[i] = false;
-			
-			fillSquare.call(this);
+		isSquareAvailable = !(player1[i] || player2[i]);
+		if (isSquareAvailable && !isGameOver) {
+			fillSquare.call(this, i);
 			isGameOver = checkForWinner(i);
-
+			if (isGameOver) {
+				celebrate();
+			}
 			// Alternate between players
 			isPlayer1Turn = !isPlayer1Turn;
 		}
 	});
 }
 
-function fillSquare() {
+function fillSquare(recentSquare) {
 	if (isPlayer1Turn) {
 		this.style.background = "lightgreen";
+		player1[recentSquare] = true;
 	} else {
 		this.style.background ="lightpink";
+		player2[recentSquare] = true;
 	}
 };
 
@@ -38,13 +42,19 @@ function checkForWinner(recentSquare) {
 	let haveWinner = false;
 	if (isPlayer1Turn) {
 		haveWinner = hasPlayerWon(player1, recentSquare);
-		player1.push(recentSquare);
 	} else {
 		haveWinner = hasPlayerWon(player2, recentSquare);
-		player2.push(recentSquare);
 	}
 	return haveWinner;
 };
+
+function celebrate() {
+	if (isPlayer1Turn) {
+		console.log(`Player 1 wins`);
+	} else {
+		console.log(`Player 2 wins`);
+	}	
+}
 
 function hasPlayerWon(squares, recentSquare) {
 	if (squares.length <= 1) return false;
@@ -66,14 +76,14 @@ function numOfHorizontalSquares(squares, recentSquare) {
 	
 	// Count up all squares that are adjacent and to the right of recentSquare
 	let possibleAdjacentSquare = recentSquare + nextSquare;
-	while (squares.includes(possibleAdjacentSquare) && col < possibleAdjacentSquare%cols) {
+	while (squares[possibleAdjacentSquare] && col < possibleAdjacentSquare%cols) {
 		adjacentSquares++;
 		possibleAdjacentSquare += nextSquare;
 	}
 
 	// Count up all squares that are adjacent and to the left of recentSquare
 	possibleAdjacentSquare = recentSquare - nextSquare;
-	while (squares.includes(possibleAdjacentSquare) && col > possibleAdjacentSquare%cols) {
+	while (squares[possibleAdjacentSquare] && col > possibleAdjacentSquare%cols) {
 		adjacentSquares++;
 		possibleAdjacentSquare -= nextSquare;
 	} 
@@ -87,14 +97,14 @@ function numOfDiagnolSquares(squares, recentSquare) {
 	
 	// Count up all squares that are adjacent and on the lower diagnol of recentSquare
 	let possibleAdjacentSquare = recentSquare + nextSquare;
-	while (squares.includes(possibleAdjacentSquare)) {
+	while (squares[possibleAdjacentSquare]) {
 		adjacentSquares++;
 		possibleAdjacentSquare += nextSquare;
 	}
 
 	// Count up all squares that are adjacent and on the upper diagnol of recentSquare
 	possibleAdjacentSquare = recentSquare - nextSquare;
-	while (squares.includes(possibleAdjacentSquare)) {
+	while (squares[possibleAdjacentSquare]) {
 		adjacentSquares++;
 		possibleAdjacentSquare -= nextSquare;
 	} 
@@ -109,7 +119,7 @@ function numOfCrossDiagnolSquares(squares, recentSquare) {
 	
 	// Count up all squares that are adjacent and on the lower cross-diagnol of recentSquare
 	let possibleAdjacentSquare = recentSquare + nextSquare;
-	while (squares.includes(possibleAdjacentSquare)) {
+	while (squares[possibleAdjacentSquare]) {
 		adjacentSquares++;
 		possibleAdjacentSquare += nextSquare;
 	}
@@ -122,7 +132,7 @@ function numOfCrossDiagnolSquares(squares, recentSquare) {
 
 	// Count up all squares that are adjacent and on the upper cross-diagnol of recentSquare
 	possibleAdjacentSquare = recentSquare - nextSquare;
-	while (squares.includes(possibleAdjacentSquare)) {
+	while (squares[possibleAdjacentSquare]) {
 		adjacentSquares++;
 		possibleAdjacentSquare -= nextSquare;
 	} 
@@ -136,14 +146,14 @@ function numOfVerticalSquares(squares, recentSquare) {
 	
 	// Count up all squares that are adjacent and on top of recentSquare
 	let possibleAdjacentSquare = recentSquare + nextSquare;
-	while (squares.includes(possibleAdjacentSquare)) {
+	while (squares[possibleAdjacentSquare]) {
 		adjacentSquares++;
 		possibleAdjacentSquare += nextSquare;
 	}
 
 	// Count up all squares that are adjacent and on the bottom of recentSquare
 	possibleAdjacentSquare = recentSquare - nextSquare;
-	while (squares.includes(possibleAdjacentSquare)) {
+	while (squares[possibleAdjacentSquare]) {
 		adjacentSquares++;
 		possibleAdjacentSquare -= nextSquare;
 	} 
